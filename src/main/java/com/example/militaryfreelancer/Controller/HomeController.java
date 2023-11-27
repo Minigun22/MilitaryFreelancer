bde0b946becb9c27fecf4546795b61177b363219
Misha Marchenko <netproblem394@gmail.com>
2023-11-27 21:54:22 +0200

    Updated

--- src/main/java/com/example/militaryfreelancer/Controller/HomeController.java
+++ src/main/java/com/example/militaryfreelancer/Controller/HomeController.java
@@ -1,17 +1,13 @@
-package com.example.militaryfreelancer;
+package com.example.militaryfreelancer.Controller;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class HelloController {
-
-    @Value("${hello_value}")
-    private String helloString;
-
-    @GetMapping("/hello")
+public class HomeController {
+    @GetMapping("/home")
     public String hello(){
-        return "hello";
+        return "home";
     }
 }
