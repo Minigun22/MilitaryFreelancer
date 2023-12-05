7a9c06a36422489a582750da110c45cd2cb43177
Jokail <jokailxx@gmail.com>
2023-12-05 12:55:48 +0200

    update Vacancy

--- src/main/java/com/example/militaryfreelancer/Models/vacany/Vacancy.java
+++ src/main/java/com/example/militaryfreelancer/Models/vacany/Vacancy.java
@@ -1,6 +1,8 @@
 package com.example.militaryfreelancer.Models.vacany;
 
 import com.example.militaryfreelancer.Models.Customer.Rank;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -18,13 +20,18 @@ public class Vacancy {
     @Size(min = 2, max = 15, message = "Довжина має бути від 2 до 15 символів")
     private String personName;
     private Rank rank;
-
+    @Email(message = "Неправильний формат електронної пошти")
+    private String email;
+    @Pattern(regexp="(^$|[0-9]{10})", message = "Неправильно вказаний номер телефону")
+    private String phone;
 
     public Vacancy(String vacancyName, String unitName, String personName, Rank rank, String email, String phone) {
         this.vacancyName = vacancyName;
         this.unitName = unitName;
         this.personName = personName;
         this.rank = rank;
+        this.email = email;
+        this.phone = phone;
     }
 
     @Override
@@ -35,6 +42,8 @@ public class Vacancy {
                 ", unitName='" + unitName + '\'' +
                 ", personName='" + personName + '\'' +
                 ", rank=" + rank +
+                ", email='" + email + '\'' +
+                ", phone='" + phone + '\'' +
                 '}';
     }
 }
