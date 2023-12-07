fc491cf133da0ac88ca6e4810bb7e4c81d3b7283
Artem Minich <temic543.9@gmail.com>
2023-12-07 18:36:11 +0200

    Create validation in CustomerController

--- src/main/java/com/example/militaryfreelancer/Controller/CustomerController.java
+++ src/main/java/com/example/militaryfreelancer/Controller/CustomerController.java
@@ -3,6 +3,7 @@ package com.example.militaryfreelancer.Controller;
 
 import com.example.militaryfreelancer.Models.Customer.Customer;
 import com.example.militaryfreelancer.Service.CustomerService;
+import com.example.militaryfreelancer.util.CustomerValidator;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -11,30 +12,37 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
-@RequestMapping("/customer")
+@RequestMapping("/customers")
 public class CustomerController {
+
     private final CustomerService customerService;
+    private final CustomerValidator customerValidator;
+
     @Autowired
-    public CustomerController(CustomerService customerService){
+    public CustomerController(CustomerService customerService, CustomerValidator customerValidator) {
         this.customerService = customerService;
+        this.customerValidator = customerValidator;
     }
+
     @GetMapping()
     public String getCustomers(Model model){
         model.addAttribute("customers", customerService.findAll());
         return "Customer/customers";
     }
-    @GetMapping("/registration")
+    @GetMapping("/new")
     public String getCreateCustomerForm(@ModelAttribute("customer") Customer customer){
-        return "Customer/registration";
+        return "Customer/new";
     }
     @PostMapping()
     public String registerCustomer(@ModelAttribute("customer") @Valid Customer customer,
                                    BindingResult bindingResult){
+        customerValidator.validate(customer, bindingResult);
+
         if(bindingResult.hasErrors()){
-            return "Customer/registration";
+            return "Customer/new";
         }
         customerService.save(customer);
-        return "redirect:/customer";
+        return "redirect:/customers";
     }
     @GetMapping("/{id}")
     public String show(@PathVariable("id") long id,Model model) {
